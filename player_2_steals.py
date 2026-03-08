e7438d895c53f33d478314c036406af732f11760
Will Boyle <willboyle18@gmail.com>
2026-03-08 14:39:59 -0700

    Implement player 2 stealing scenario

---
+++ player_2_steals.py
@@ -0,0 +1,45 @@
+import generate_bags
+import random
+
+def print_final_results(wins):
+    print("\nFinal Simulation Results")
+    print("Player 1 Win Total:", wins[1])
+    print("Player 2 Win Total:", wins[2])
+    print("Player 3 Win Total:", wins[3])
+
+def player_2_steals(sims):
+    bags = generate_bags.generate_bags(sims)
+    print("Running simulation where player 2 steals from player 1 and player 3 takes from the bag")
+
+    wins = {1: 0, 2: 0, 3: 0}
+    
+    for bag in bags:
+        # Player 1 Turn
+        rock = random.randint(0, 2)
+        player_1 = bag.pop(rock)
+
+        # Player 2 Turn
+        player_2 = player_1            
+
+        # Player 1 Goes again
+        rock = random.randint(0, 1)
+        player_1 = bag.pop(rock)
+
+        # Player 3 Turn
+        player_3 = bag.pop()
+
+        if player_1 == 1:
+            print("Player 1 wins")
+            wins[1] += 1
+        elif player_2 == 1:
+            print("Player 2 wins")
+            wins[2] += 1
+        elif player_3 == 1:
+            print("Player 3 wins")
+            wins[3] += 1
+    
+    print_final_results(wins)
+
+
+
+player_2_steals(10000)
